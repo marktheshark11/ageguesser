5504a8a610d6c216b854721550d4f329a43f3f33
DaWiltz <157007660+DaWiltz@users.noreply.github.com>
2025-05-05 18:18:34 +0200

    Sista fix av kategorier

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ dataset = []
 [gender] is either 0 (male) or 1 (female)
 [race] is an integer from 0 to 4, denoting White, Black, Asian, Indian, and Others (like Hispanic, Latino, Middle Eastern).
 [date&time] is in the format of yyyymmddHHMMSSFFF, showing the date and time an image was collected to UTKFace
-https://integrishealth.org/resources/on-your-health/2015/october/stages-of-life-health-for-every-age
-Infant = 0-1 year
+
+Age Categories:
 Baby = 1 year
 Toddler = 2-4 yrs
 Child = 5-12 yrs
@@ -20,6 +20,7 @@ Teen = 13-19 yrs
 Adult = 20-39 yrs
 Middle Age Adult = 40-59 yrs
 Senior Adult = 60+
+
 """
 
 def agegroup(age):
@@ -39,6 +40,25 @@ def agegroup(age):
         return "Middle Age Adult"
     else:
         return "Senior Adult"
+
+def int_to_gender(gender_int):
+    if gender_int == 0:
+        return 'Male'
+    elif gender_int == 1:
+        return 'Female'
+
+def int_to_race(race_int):
+    if race_int == 0:
+        return 'White'
+    elif race_int == 1:
+        return 'Black'
+    elif race_int == 2:
+        return 'Asian'
+    elif race_int == 3:
+        return 'Indian'
+    elif race_int == 4:
+        return 'Others'
+    
 for file in os.listdir('UTKFace'):
     if file.endswith('.jpg'):
         # Split the filename into its components
@@ -50,12 +70,15 @@ for file in os.listdir('UTKFace'):
         gender = int(parts[1])
         race = int(parts[2])
         date_time = parts[3].split('.')[0]
-        dataset.append({"filename": file, "age": age, "gender": gender, "race": race, "date_time": date_time})
+        dataset.append({"Filename": file, "Age": age, "Gender": gender, "Race": race, "Date_time": date_time})
 df = pd.DataFrame(dataset)
 
-df['age_group'] = df['age'].apply(agegroup)
+# För att konvertera ints till strängar i df
+df['Age_group'] = df['Age'].apply(agegroup)
+df['Gender'] = df['Gender'].apply(int_to_gender)
+df['Race'] = df['Race'].apply(int_to_race)
 
-print("Total images loaded:", len(df))
-print(df.head())
+print("Total image metadata loaded:", len(df))
+print(df.head().drop(columns=['Filename', 'Date_time']))
 
 
